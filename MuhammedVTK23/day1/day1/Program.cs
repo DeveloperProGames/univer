0caeba96159c1b68c499ffcad8128b797558e86c
DeveloperProGames <135852385+DeveloperProGames@users.noreply.github.com>
2024-02-26 16:40:20 +0600

    Add files via upload

---
+++ MuhammedVTK23/day1/day1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "q";
+            while (a != "0" || a != "") {
+                Console.Write("Zadacha: ");
+                a = Console.ReadLine();
+                switch (a)
+                {
+                    case "1":
+                        one(); break;
+                    default:
+                        Console.WriteLine("Ne pravilni");
+                        break;
+
+                }
+
+            }
+
+        }
+        static void one()
+        {
+            Console.Write("Kids: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Apples: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(k / n);
+        }
+    }
+}
